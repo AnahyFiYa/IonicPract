18bbf86eb236d8ab632d6921da86c558b39220c0
Anahy FiYa <anahyfigueroaynz@gmail.com>
2019-06-03 11:35:42 -0700

    Cambios Prueba

---
+++ src/pages/chores/choreslist.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'page-choreslist',
+    templateUrl: 'choreslist.html'
+})
+export class ChoresList {
+    constructor(){}
+}
