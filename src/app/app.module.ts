86e9af0ded55d0d53eef7640d4de59aa93fe83af
Anahy FiYa <anahyfigueroaynz@gmail.com>
2019-06-03 11:49:56 -0700

    Cambios Views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { ChoresList } from '../pages/chores/choreslist';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,8 +17,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    ChoresList
+    TabsPage
   ],
   imports: [
     BrowserModule,
